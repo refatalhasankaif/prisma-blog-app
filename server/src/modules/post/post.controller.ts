e483a3607d83de69979469b67b1da71b12544dd9
Refat al hasan <refatalhasan911@gmail.com>
2026-01-08 01:07:31 +0600

    module 23 done

--- server/src/modules/post/post.controller.ts
+++ server/src/modules/post/post.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
+import { PostService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
-    console.log({ req, res })
-    res.send("create a new post")
+    try {
+        const result = await PostService.createPost(req.body)
+        res.status(201).json(result)
+    } catch (err) {
+        res.status(400).json({
+            error: "Post creation failed",
+            details: err
+        })
+    }
 }
 
 export const PostControler = {
