75f80ebb1e1855cf9143868a80725dbc00afc8f0
Refat al hasan <refatalhasan911@gmail.com>
2026-01-08 18:57:00 +0600

    vidoe 1 complete

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,18 @@
 import express, { Application } from "express";
+import { toNodeHandler } from "better-auth/node";
 import { postRouter } from "./modules/post/post.router";
+import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:4000",
+    credentials: true,
+    
+}))
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.use(express.json());
 
@@ -12,3 +23,4 @@ app.get("/", (req, res) => {
 });
 
 export default app;
+
